46cfa96fddb5c8a03a978e1cf1cac45ce756dd71
Julia D D <98531822+juliasteckler@users.noreply.github.com>
2022-02-22 02:58:25 +0000

    Raced two turtles in two races, and one turtle draws five shapes.

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Two turtles are raced using random values, and one draws 5 shapes. 
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 3/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+
 
 ## REFERENCES:
- < List any outside resources used >
+ 
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+
--- main.py
+++ main.py
@@ -18,9 +18,41 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
-
-
+x = random.randrange(1, 100)
+y = random.randrange(1, 100)
+leonardo.forward(x)
+michelangelo.forward(y)
+leonardo.goto(-100,-20)
+michelangelo.goto(-100,20)
+for i in range(10):
+  a = random.randrange(0, 10)
+  b = random.randrange(0, 10)
+  for i in range(10):
+    leonardo.forward(a)
+    michelangelo.forward(b)
+leonardo.goto(-100,-20)
+michelangelo.goto(-100,20)
 # Part B - complete part B here
-
-
+leonardo.down()
+for i in range(3):
+  leonardo.forward(30)
+  leonardo.right(360/3)
+leonardo.clear()
+for i in range(4):
+  leonardo.forward(30)
+  leonardo.right(360/4)
+leonardo.clear()
+for i in range(6):
+  leonardo.forward(20)
+  leonardo.right(360/6)
+leonardo.clear()
+for i in range(9):
+  leonardo.forward(20)
+  leonardo.right(360/9)
+leonardo.clear()
+for i in range(12):
+  leonardo.forward(20)
+  leonardo.right(360/12)
+leonardo.clear()
+leonardo.up()
 window.exitonclick()
